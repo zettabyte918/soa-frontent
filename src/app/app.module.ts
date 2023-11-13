46fabe460e5bcc506f6a6377a7538991f12024f7
Hossem <hammamihossem99@gmail.com>
2023-11-13 23:43:39 +0100

    add env and http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './authentication/login/login.component';
@@ -14,7 +14,8 @@ import { RegisterComponent } from './authentication/register/register.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/author.service.ts
+++ src/app/services/author.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Author } from '../models/Author';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorService {
+  authors: any = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  GetAuthors() {
+    return this.http.get<Author[]>(`${environment.api_url}/authors`);
+  }
 }
